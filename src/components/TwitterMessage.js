eb7d58980c2d584f169e948b6964250db46cb4a9
AJ Guiles <ashiguiles@gmail.com>
2020-05-06 01:00:15 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -22,7 +22,7 @@ handleChange = e => {
         <input type="text" name="message" id="message" value = {this.state.value} onChange ={this.handleChange}  />
                     {/* connecets state to message, when input changes call handleChange */}
                     <P> {this.props.maxChars-this.state.value.length}</p>
-                    takes in the prop of maxChars from index js and subtract the the string length of state.value(aka user input) 
+                    {takes in the prop of maxChars from index js and subtract the the string length of state.value(aka user input) }
       </div>
     );
   }
