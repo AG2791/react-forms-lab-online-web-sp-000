355e9639be51dc798a87c4e89cd18ca46957ff58
AJ Guiles <ashiguiles@gmail.com>
2020-05-06 00:55:18 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -21,6 +21,7 @@ handleChange = e => {
         <strong>Your message:</strong>
         <input type="text" name="message" id="message" value = {this.state.value} onChange ={this.handleChange}  />
                     {/* connecets state to message, when input changes call handleChange */}
+                    <P>
       </div>
     );
   }
