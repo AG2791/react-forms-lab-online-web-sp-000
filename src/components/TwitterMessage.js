a77cde11e996a346986dcbcd4678ec9124e3b012
AJ Guiles <ashiguiles@gmail.com>
2020-05-06 00:46:41 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -12,7 +12,7 @@ class TwitterMessage extends React.Component {
 handleChange = e => {
   this.setState({
     value: e.target.value
-    //target is the input element
+    //target is the input element value
   })
 }
   render() {
